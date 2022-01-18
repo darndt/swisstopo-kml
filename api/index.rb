6f7dfe905179d81c6cd1416b02a0bb688a3bb27e
Daniel Arndt <darndt@gmail.com>
2022-01-18 13:35:44 +0100

    dev

--- api/index.rb
+++ api/index.rb
@@ -1,11 +1,10 @@
 Handler = Proc.new do |req, res|
   res.status = 200
   res['Content-Type'] = 'text/text; charset=utf-8'
-  # if req.query.has_key?("name")
-  #   name = req.query["name"]
-  #   res.body = "Hello, #{name}!"
-  # else
-  #   res.body = "Hello, stranger!"
-  # end
-  res.body = req.query.to_s
+  if req.query.has_key?("name")
+    name = req.query["name"]
+    res.body = Cowsay.say("Hello #{name}", 'cow')
+  else
+    res.body = "Hello, stranger!"
+  end
 end
