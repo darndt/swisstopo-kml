2414567248441345ce01e6789ec8d17d09a94f40
Daniel Arndt <darndt@gmail.com>
2022-01-19 11:44:37 +0100

    dev

---
+++ mercator.rb
@@ -0,0 +1,29 @@
+class MercatorProjection
+  @@MERCATOR_RANGE = 256
+  @@pixelOrigin_x = @@MERCATOR_RANGE / 2.0
+  @@pixelOrigin_y = @@MERCATOR_RANGE / 2.0
+  @@pixelsPerLonDegree = @@MERCATOR_RANGE / 360.0
+  @@pixelsPerLonRadian = @@MERCATOR_RANGE / (2 * Math::PI)
+
+  def self.fromLatLngToPoint(lat, lng) 
+    x = 128 + lng * @@pixelsPerLonDegree
+    # NOTE(appleton): Truncating to 0.9999 effectively limits latitude to
+    # 89.189.  This is about a third of a tile past the edge of the world tile.
+    siny = [[Math::sin(lat*Math::PI/180.0), -0.9999].max, 0.9999].min
+    y = 128 + 0.5 * Math::log((1 + siny) / (1 - siny)) * (-1.0*@@pixelsPerLonRadian)
+    return [x, y]
+  end
+ 
+  def self.fromPointToLatLng(x, y)
+    lng = (x - 128) / @@pixelsPerLonDegree
+    latRadians = (y - 128) / (-1.0 * @@pixelsPerLonRadian)
+    lat = (2 * Math.atan(Math::exp(latRadians)) - Math::PI / 2) * 180 / Math::PI
+    return [lat, lng]
+  end
+  
+  # calculates the coordinate of the upper left corner of the tile
+  def self.fromTileXYZoomToLatLng(tile_x, tile_y, zoomlevel)
+    return self.fromPointToLatLng(tile_x*256.0/2**zoomlevel, tile_y*256.0/2**zoomlevel)
+  end
+  
+end
--- web.rb
+++ web.rb
@@ -1,5 +1,39 @@
 require 'sinatra'
+require "erb"
+require "./mercator"
 
 get '/' do
   'Hello world!'
+end
+
+get '/api' do
+  tiles = {
+    8 => {:tile_x_start => 131, :tile_x_end => 136, :tile_y_start => 88, :tile_y_end => 92},
+    9 => {:tile_x_start => 262, :tile_x_end => 271, :tile_y_start => 177, :tile_y_end => 183},
+    10 => {:tile_x_start => 528, :tile_x_end => 541, :tile_y_start => 356, :tile_y_end => 365},
+    11 => {:tile_x_start => 1057, :tile_x_end => 1083, :tile_y_start => 713, :tile_y_end => 730},
+    12 => {:tile_x_start => 2115, :tile_x_end => 2167, :tile_y_start => 1427, :tile_y_end => 1460},
+    13 => {:tile_x_start => 4231, :tile_x_end => 4335, :tile_y_start => 2854, :tile_y_end => 2921},
+    14 => {:tile_x_start => 8462, :tile_x_end => 8670, :tile_y_start => 5708, :tile_y_end => 5842},
+    15 => {:tile_x_start => 16925, :tile_x_end => 17341, :tile_y_start => 11416, :tile_y_end => 11684},
+  }
+
+  # template = File.dirname(__FILE__) + '/api/kml_template.rhtml'
+
+  # erb = ERB.new(File.read(template).gsub(/^\s+/, ""), 0, "%<>")
+
+  if params[:z].nil?
+    x_range = tiles[8][:tile_x_start]..tiles[8][:tile_x_end]
+    y_range = tiles[8][:tile_y_start]..tiles[8][:tile_y_end]
+    erb :kml, :locals => {:x_range => x_range, :y_range => y_range, :z => 8}
+  else
+
+    z = params[:z].to_i  # assumes z>=9
+    x, y = params[:x].to_i, params[:y].to_i
+
+    x_range = (x*2)..(x*2+1)
+    y_range = (y*2)..(y*2+1)
+    erb :kml, :locals => {:x_range => x_range, :y_range => y_range, :z => z+1}
+  end
+
 end
